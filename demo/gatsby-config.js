00f64203e0a0518b8108fc18d2ca3fba4b1aec90
Justin Palpant <justin@palpant.us>
2020-05-26 00:46:41 -0700

    Add plugin and use personal siteConfig.js.
    Not checking in my content API key.

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -76,6 +76,9 @@ module.exports = {
                     {
                         resolve: `gatsby-rehype-prismjs`,
                     },
+                    {
+                        resolve: `gatsby-rehype-inline-images`,
+                    },
                 ],
             },
         },
--- demo/siteConfig.js
+++ demo/siteConfig.js
@@ -1,27 +1,24 @@
 module.exports = {
     // Do not include a trailing slash!
-    siteUrl: `https://styxlab.github.io`,
+    siteUrl: `https://justin.palpant.us`,
 
     // Maximum number of post shown per page
     // With Infinite Scroll this is the chunk of posts fetched when scrolling down
     postsPerPage: 3,
 
     // This allows an alternative site title for meta data for pages.
-    siteTitleMeta: `Gatsby Ghost Casper`,
+    siteTitleMeta: `Justin Palpant - a star shines upon the hour of our meeting`,
 
     // This allows an site description for meta data for pages.
-    siteDescriptionMeta: `Gastby with Ghost CMS and Casper Theme`,
+    siteDescriptionMeta: `My personal blog`,
 
     // Change to the width and height of your default share image
     shareImageWidth: 1000,
     shareImageHeight: 523,
 
     // Used for App and Offline manifest e.g. Mobile Home Screen
-    shortTitle: `Jamify`,
-    siteIcon: `favicon.png`,
+    shortTitle: `Justin Palpant Blog`,
+    siteIcon: `favicon_128.png`,
     backgroundColor: `#e9e9e9`,
     themeColor: `#15171A`,
-
-    // Overwrite navigation menu (default: []), label is case sensitive
-    // overwriteGhostNavigation: [{ label: `Home`, url: `/` }],
 }
