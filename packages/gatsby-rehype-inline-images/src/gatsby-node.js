84ca13b913f1f1090643a1c3df83af625437e9fd
Justin Palpant <justin@palpant.us>
2020-05-26 01:07:45 -0700

    Comment out invalid syntax and add some WIP comments.

--- packages/gatsby-rehype-inline-images/src/gatsby-node.js
+++ packages/gatsby-rehype-inline-images/src/gatsby-node.js
@@ -37,7 +37,7 @@ module.exports = async ({
     reporter.info(`CMS URL is ${cmsUrl}`)
 
     visit(htmlAst, { tagName: `img` }, (node) => {
-        // get all <img \> tags and their src property
+        // get all <img /> tags and their src property
         const src = node.properties && node.properties.src
 
         // if the image source is an uploaded image at the Ghost CMS URL, download it
@@ -47,19 +47,30 @@ module.exports = async ({
             let fileNode
             try {
                 // can't use await inside synchronous function. Will have to redo this.
-                // also, how to get the static URL to the saved image?
-                fileNode = await createRemoteFileNode({
-                    url: src,
-                    parentNodeId: node.id,
-                    createNode,
-                    createNodeId,
-                    cache,
-                    store,
-                })
+                // consider interacting with cache the way gatsby-remark-images-anywhere does https://github.com/d4rekanguok/gatsby-remark-images-anywhere/blob/master/src/util-download-image.ts#L18-L57
+                // fileNode = await createRemoteFileNode({
+                //     url: src,
+                //     parentNodeId: node.id,
+                //     createNode,
+                //     createNodeId,
+                //     cache,
+                //     store,
+                // })
             } catch (e){
                 reporter.warn(`Remote image failure.`)
                 return
             }
+            // process the image with sharp
+            // Example: https://github.com/d4rekanguok/gatsby-remark-images-anywhere/blob/master/src/util-download-image.ts#L62
+            // sharpMethod can be ('fluid', 'fixed', or 'resize'); gatsby-remark-images-anywhere does config for this in index.js: https://github.com/d4rekanguok/gatsby-remark-images-anywhere/blob/master/src/index.ts
+
+            // Then, we need a local path to the static image.
+            // gatsby-plugin-ghost-images seems to produce <picture /> elements, but the image sources are /static/{long random string}/{short random string}/{original filename}
+            // but that's all baked into the React component and GraphQL, I think.
+
+            // const relImageSrcUrl = ""
+            const relImageSrcUrl = null;
+            node.properties.src = relImageSrcUrl ? relImageSrcUrl : 'https://placekitten.com/1200/800'
         }
     })
 
